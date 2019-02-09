803e0aca22698d2eb4c0ee2fe2ae7c5c61c4c9a0
Alexander Cleoni <alexander.cleoni@gmail.com>
2019-02-09 01:27:23 -0500

    Refactored api url

--- src/private/index.js
+++ src/private/index.js
@@ -1,5 +1,5 @@
 export const API_DEV = 'http://localhost:3000';
-export const API_PROD = 'https://api.mpcrown.com';
+export const API_PROD = 'https://mpcrown-api.herokuapp.com/';
 
 export const STRIPE_KEY_PROD = 'pk_live_emTYUveN4SC7GPNwVljA8TlM';
 export const STRIPE_KEY_DEV = 'pk_test_fYvky3ngOBe5ejxXazkEj4Oo';
